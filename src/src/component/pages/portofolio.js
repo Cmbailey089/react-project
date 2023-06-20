97f985246cfbbee7094767fca136c6e74b879063
Cmbailey089 <christopherbailey089@gmail.com>
2023-06-20 18:16:33 -0400

    created image

--- src/src/component/pages/portofolio.js
+++ src/src/component/pages/portofolio.js
@@ -1,9 +1,10 @@
 import React from "react";
+import planner from "../img/planner.jpg";
 
 export default function portfolio() {
     return(
         <div className="portfolio">
-            
+            <img src={planner} href="https://github.com/Cmbailey089/work-planner"/>
         </div>
     )
 }
